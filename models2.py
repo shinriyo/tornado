1e3a6e25fa1806d843c3adb0439f23a2d5819bd4
shinriyo <shinriyo@gmail.com>
2012-03-29 12:22:00 +0900

    modify

---
+++ models2.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# inherit DeclarativeBase class when you write up a model.
+DeclarativeBase = declarative_base()
+metadata = DeclarativeBase.metadata
+
+# set the connection string here
+engine = create_engine('sqlite:///example2.db')
+Session = sessionmaker(bind=engine, autocommit=True)
+
+# import the dbsession instance to execute queries on your database
+dbsession = Session()
+
+# import your models.
+#from models.auth import Group, Permission, User
+
+# calling this will create the tables at the database
+#__create__ = lambda: (setattr(engine, 'echo', True), metadata.create_all(engine))
+
